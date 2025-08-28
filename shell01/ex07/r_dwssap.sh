9be24d1b40808949173ac3d162844742ca532a13
Martin Padron Rodriguez <mapadron@c2r17s2.42madrid.com>
2025-08-28 19:43:44 +0200

    added shell00 & 01

---
+++ shell01/ex07/r_dwssap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cat /etc/passwd | \
+cut -d: -f1 | \
+awk 'NR %2 == 0'| \
+rev | \
+sort -r | \
+awk -v start="%FT_LINE1" -v end="$FT_LINE2" 'NR>=start && NR<=end' | \
+tr '\n' ',' | \
+sed 's/,$/./g' | \
+sed 's/,/, /g'
